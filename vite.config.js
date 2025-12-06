d50d54217d71a5500922e6d19ab6df2a5c2c14ab
Ashutosh Ranjan <iaashu98@gmail.com>
2025-12-07 00:25:05 +0530

    updated dist

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: '/',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        // Ensure that public directory is copied
+        copyPublicDir: true,
+    },
+    server: {
+        open: true,
+    }
+});
